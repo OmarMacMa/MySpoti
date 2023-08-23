12c56b58a7ab628cc1022106521a46361e751cdd
Omar Macias <a01635795@tec.mx>
2023-08-22 18:39:48 -0600

    docs, QA(main.py): Added docstrings for the main.py and improved the code quality, naming conventions and more self explanatory code

--- MySpoti/main.py
+++ MySpoti/main.py
@@ -1,54 +1,73 @@
-import datetime
-import html
 import os
-import time
 import spotipy
-from flask import json, render_template, session, request, redirect
+from flask import render_template, session, request, redirect
 from .app import create_app
-# from keys import Client_ID, Client_Secret, Redirect_URI
-# from flask_session import Session
 
 
-app = create_app()
+app = create_app()  # Create Flask app with the configuration from config.py
 
 
 def validate_time_range(time_range):
     """
-    Validate time range and return the amount of time
+    Validate time range and convert it to a more readable format
+
+    Args:
+        time_range (str): The time range to validate
+
+    Returns:
+        Tuple[str, str]: The time range and the amount of time it represents
     """
     time_ranges_map = {
-    "long_term": "several years",
-    "medium_term": "last 6 months",
-    "short_term": "last 4 weeks"
+        "long_term": "several years",
+        "medium_term": "last 6 months",
+        "short_term": "last 4 weeks"
     }
-    if time_range != "short_term" and time_range != "medium_term" and time_range != "long_term":
+    if time_range not in time_ranges_map:
         time_range = "short_term"
     return time_range, time_ranges_map[time_range]
 
 
 def validate_limit(limit):
     """
-    Validate limit
+    Validate limit and convert it to a valid limit
+
+    Args:
+        limit (int): The limit to validate
+
+    Returns:
+        int: The valid limit
     """
-    if limit != 12 and limit != 24 and limit != 50 and limit != 48:
+    valid_limits = {12, 24, 48, 50}
+    if limit not in valid_limits:
         return 12
     return limit
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """
+    404 error page handler for page not found
+    """
     return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_error(e):
+    """
+    500 error page handler for internal server error
+    """
     return render_template("500.html"), 500
 
 
 @app.route("/")
 def index():
     """
-    Home page
+    Render the welcome page
+
+    Returns:
+        template: The index.html template
+        Context:
+            - signed_in: Whether the user is signed in or not
     """
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(
@@ -70,11 +89,18 @@ def index():
 @app.route("/home")
 def home():
     """
-    User login and authorization with Spotify API
+    Handle the authentication process with Spotify and render the home page
+
+    Returns:
+        - redirect: Redirects to the home page if user authenticated
+        - redirect: Redirects to the Spotify auth if user not authenticated
+        Context:
+            - user: The user's information
+            - signed_in: Whether the user is signed in or not
     """
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(
-        client_id=os.environ.get("Client_ID"), 
+        client_id=os.environ.get("Client_ID"),
         client_secret=os.environ.get("Client_Secret"),
         redirect_uri=os.environ.get("Redirect_URI"),
         scope="user-read-currently-playing user-top-read user-read-recently-played",
@@ -125,6 +151,19 @@ def currently_playing():
 
 @app.route("/top_tracks")
 def top_tracks():
+    """
+    Render the top tracks page
+
+    Returns:
+        - template: The top_tracks.html template
+        - redirect: Redirects to the home page if user not authenticated
+        Context:
+            - top_tracks: A dictionary of the user's top tracks information
+            - time_range: The time range of the top tracks
+            - limit: The limit of the top tracks
+            - time: The amount of time the time range represents
+            - signed_in: Whether the user is signed in or not
+        """
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(
         client_id=os.environ.get("Client_ID"),
@@ -136,21 +175,26 @@ def top_tracks():
         return redirect("/home")
     else:
         signed_in = True
-    time_range, time = validate_time_range(request.args.get("time_range", "short_term"))
+    time_range, time = validate_time_range(
+        request.args.get("time_range", "short_term")
+    )
     limit = validate_limit(int(request.args.get("limit", 12)))
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    top_tracks_list = spotify.current_user_top_tracks(limit=limit, time_range=time_range)
-    top_tracks = {}
+    top_tracks_list = spotify.current_user_top_tracks(
+        limit=limit, time_range=time_range
+    )
+    top_tracks_json = {}
     for i, item in enumerate(top_tracks_list["items"]):
-        top_tracks[i] = {
+        artists = [artist["name"] for artist in item["artists"]]
+        top_tracks_json[i] = {
             "name": item["name"],
-            "artists": ", ".join([artist["name"] for artist in item["artists"]]),
+            "artists": ", ".join(artists),
             "album": item["album"]["name"],
             "album_cover": item["album"]["images"][1]["url"],
             "url": item["external_urls"]["spotify"]
         }
     context = {
-        "top_tracks": top_tracks,
+        "top_tracks": top_tracks_json,
         "time_range": time_range,
         "limit": limit,
         "time": time,
@@ -161,6 +205,19 @@ def top_tracks():
 
 @app.route("/top_artists")
 def top_artists():
+    """
+    Render the top artists page
+
+    Returns:
+        - template: The top_artists.html template
+        - redirect: Redirects to the home page if user not authenticated
+        Context:
+            - top_artists: A dictionary of the user's top artists information
+            - time_range: The time range of the top artists
+            - limit: The limit of the top artists
+            - time: The amount of time the time range represents
+            - signed_in: Whether the user is signed in or not
+    """
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(
         client_id=os.environ.get("Client_ID"),
@@ -172,20 +229,24 @@ def top_artists():
         return redirect("/home")
     else:
         signed_in = True
-    time_range, time = validate_time_range(request.args.get("time_range", "short_term"))
+    time_range, time = validate_time_range(
+        request.args.get("time_range", "short_term")
+    )
     limit = validate_limit(int(request.args.get("limit", 12)))
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    top_artists_list = spotify.current_user_top_artists(limit=limit, time_range=time_range)
-    top_artists = {}
+    top_artists_list = spotify.current_user_top_artists(
+        limit=limit, time_range=time_range
+    )
+    top_artists_json = {}
     for i, item in enumerate(top_artists_list["items"]):
-        top_artists[i] = {
+        top_artists_json[i] = {
             "name": item["name"],
             "genres": ", ".join(item["genres"]),
             "image": item["images"][1]["url"],
             "url": item["external_urls"]["spotify"]
         }
     context = {
-        "top_artists": top_artists,
+        "top_artists": top_artists_json,
         "time_range": time_range,
         "limit": limit,
         "time": time,
@@ -196,6 +257,17 @@ def top_artists():
 
 @app.route("/recently_played")
 def recently_played():
+    """
+    Render the recently played page
+
+    Returns:
+        - template: The recently_played.html template
+        - redirect: Redirects to the home page if user not authenticated
+        Context:
+            - recently_played: The user's recently played tracks information
+            - limit: The limit of the recently played tracks
+            - signed_in: Whether the user is signed in or not
+    """
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(
         client_id=os.environ.get("Client_ID"),
@@ -207,23 +279,34 @@ def recently_played():
         return redirect("/home")
     else:
         signed_in = True
+    limit = validate_limit(int(request.args.get("limit", 12)))
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    recently_played_list = spotify.current_user_recently_played(limit=50)
-    # return recently_played_list
-    tracks = ""
-    for i in range(len(recently_played_list["items"])):
-        tracks += f'<h2><a href="{recently_played_list["items"][i]["track"]["external_urls"]["spotify"]}">{recently_played_list["items"][i]["track"]["name"]}</a></h2>'
-        tracks += f'<h3>by {", ".join([artist["name"] for artist in recently_played_list["items"][i]["track"]["artists"]])}</h3>'
-        tracks += f'<h3>from <a href="{recently_played_list["items"][i]["track"]["album"]["external_urls"]["spotify"]}">{recently_played_list["items"][i]["track"]["album"]["name"]}</a></h3>'
-        tracks += f'<img src="{recently_played_list["items"][i]["track"]["album"]["images"][0]["url"]}" alt="album cover" width="300" height="300">'
-    return tracks
-
+    recently_played_list = spotify.current_user_recently_played(limit=limit)
+    recently_played_json = {}
+    for i, item in enumerate(recently_played_list["items"]):
+        artists = [artist["name"] for artist in item["track"]["artists"]]
+        recently_played_json[i] = {
+            "name": item["track"]["name"],
+            "artists": ", ".join(artists),
+            "album": item["track"]["album"]["name"],
+            "album_cover": item["track"]["album"]["images"][1]["url"],
+            "url": item["track"]["external_urls"]["spotify"]
+        }
+    context = {
+        "recently_played": recently_played_json,
+        "limit": limit,
+        "signed_in": signed_in
+    }
+    return render_template("recently_played.html", **context)
 
 
 @app.route("/sign_out")
 def sign_out():
     """
-    Sign out from Spotify
+    Sign out the user
+
+    Returns:
+        - redirect: Redirects to the home page after signing out
     """
     session.pop("token_info", None)
     return redirect("/")
